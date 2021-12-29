ecb28e25825b550d1503a00f8fb1715985efb923
Jonathan Chiquet <34842061+JonathanChi21@users.noreply.github.com>
2021-12-29 14:15:13 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,10 +14,4 @@ const server = new ApolloServer({
   },
 });
 
-server.listen().then(() => {
-  console.log(`
-    🚀  Server is running!
-    🔉  Listening on port 4000
-    📭  Query at https://studio.apollographql.com/dev
-  `);
-});
+await server.listen({port: process.env.PORT || 4000});
